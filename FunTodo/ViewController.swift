291f7d13dfe931eaecef27f0a6ab5fadeac028f5
Jeffrey Nelson Jr <neljstytretja@gmail.com>
2019-07-21 12:41:32 -0600

    Initial Commit

---
+++ FunTodo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FunTodo
+//
+//  Created by Jrnels02 on 7/21/19.
+//  Copyright © 2019 Jrnels02. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
